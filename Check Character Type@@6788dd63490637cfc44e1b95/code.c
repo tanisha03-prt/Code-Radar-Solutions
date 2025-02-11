6885d7031290281e1938e2497a6ef238095904b1
tanisha03-prt <tanishasingh0302@gmail.com>
2025-02-11 11:50:20 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    ch c;
+    scanf("%c",&ch);
+    if(ch=='aeiou'){
+        printf("Vowel");
+    } else if{
+        printf("Consonant");
+    } else{
+        printf("Digit");
+    }
+    return 0;
+}
